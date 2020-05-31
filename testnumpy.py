a2e3d31f10bdab7a4bf6cc37eae4cc277f76d4fe
Titash Roy <titash.roy@yash.com>
2020-05-31 13:54:47 +0530

    final 31052020

--- testnumpy.py
+++ testnumpy.py
@@ -1,11 +1,24 @@
 import numpy as np
 import json
 
-print(np.array([1, 2, 3]) * np.array([4, 5, 6]))
+a = np.array([1, 2, 3]) * np.array([4, 5, 6])
+b = np.array([1, 2, 3, 4, 5], ndmin=4)
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-# --converts a python object into JSON
-print(json.dumps({'id': '1', 'name': 'Andrew'})) 
+print(a)
+print(b)
+print(c)
+print(d)
+# print(type(a))
+print(a.ndim) 
+print(b.ndim) 
+print(c.ndim) 
+print(d.ndim) 
 
-# some JSON to python
-x =  '{ "name":"John", "age":30, "city":"New York"}'
-print(json.loads(x))
+# # --converts a python object into JSON
+# print(json.dumps({'id': '1', 'name': 'Andrew'})) 
+
+# # some JSON to python
+# x =  '{ "name":"John", "age":30, "city":"New York"}'
+# print(json.loads(x))
